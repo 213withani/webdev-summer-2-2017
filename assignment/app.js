5a495376dce8ece3675d0422e234f1646e9a86ba
Israel <213withani@gmail.com>
2017-10-02 21:28:59 -0700

    Declared our first controller.

---
+++ assignment/app.js
@@ -0,0 +1,13 @@
+// (function() {
+
+// })();
+
+// Bind the HTML. Declare a variable to be reused.
+var app = angular.module("WampApp", []);
+
+// Binding the view with the controller: View notify the controller of its events.
+app.controller("loginController", loginController);
+
+function loginController() {
+
+}
