bb6055b3f1054722fd3fe86d6cc5ef65b83bb038
Israel <213withani@gmail.com>
2017-10-02 21:47:59 -0700

    ng model updates whoever is listening

--- assignment/app.js
+++ assignment/app.js
@@ -5,7 +5,8 @@
 // Bind the HTML. Declare a variable to be reused.
 var app = angular.module("WampApp", []);
 
-// Binding the view with the controller: View notify the controller of its events.
+// Binding the view with the controller: 
+// View notify the controller of its events.
 app.controller("loginController", loginController);
 
 function loginController($scope) {
