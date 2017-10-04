067c3713e39b6f7005047cdf7b66518d5e2abe07
Israel <213withani@gmail.com>
2017-10-03 20:52:55 -0700

    created routing for login.html

--- assignment/app.js
+++ assignment/app.js
@@ -1,26 +1,30 @@
-// (function() {
+(function () {
+    // Bind the HTML. Declare a variable to be reused.
+    var app = angular.module("WampApp", ["ngRoute"]);
 
-// })();
+    // Binding the view with the controller: 
+    // View notify the controller of its events.
+    app.controller("loginController", loginController);
+    app.config(configuration);
 
-// Bind the HTML. Declare a variable to be reused.
-var app = angular.module("WampApp", []);
+    function configuration($routeProvider) {
+        $routeProvider.when("/login",{
+            templateUrl:"login.html"
+        })
+    }
+    function loginController($scope) {
+        var users = [{ _id: 123, username: "alice", password: "alice", firstName: "Alice", lastName: "Wonderland" },
+        { _id: 234, username: "bob", password: "bob", firstName: "Bob", lastName: "Bob" },
+        ];
 
-// Binding the view with the controller: 
-// View notify the controller of its events.
-app.controller("loginController", loginController);
+        $scope.login = function (user) {
+            for (var u in users) {
+                var _user = users[u];
 
-function loginController($scope) {
-    var users = [{_id:123, username:"alice", password:"alice", firstName:"Alice", lastName:"Wonderland"},
-    {_id:234, username:"bob", password:"bob", firstName:"Bob", lastName:"Bob"},
-    ];
-
-    $scope.login =  function (user) {
-        for (var u in users) {
-            var _user=users[u];
-
-            if (_user.username===user.username && _user.password===user.password ) {
-                 $scope.welcomeUser = _user;
+                if (_user.username === user.username && _user.password === user.password) {
+                    $scope.welcomeUser = _user;
+                }
             }
         }
     }
-}
+})();
