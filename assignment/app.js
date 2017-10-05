a86a71e7911fd83adc5a68721e6d1aad2c96da93
Israel <213withani@gmail.com>
2017-10-04 20:24:13 -0700

    Connecting the login page with the correct user.

--- assignment/app.js
+++ assignment/app.js
@@ -5,20 +5,35 @@
     // Binding the view with the controller: 
     // View notify the controller of its events.
     app.controller("loginController", loginController);
+    app.controller("profileController", profileController);
     app.config(configuration);
 
+    var users = [
+        { _id: 123, username: "alice", password: "alice", firstName: "Alice", lastName: "Wonder" },
+        { _id: 234, username: "bob", password: "bob", firstName: "Bob", lastName: "Bob" },
+    ];
+
+    function profileController($scope, $routeParams) {
+        var userId = $routeParams.userId;
+        for (var u in users) {
+            if (users[u]._id === userId) {
+                $scope.user = users[u];
+            }
+        }
+    }
     function configuration($routeProvider) {
-        $routeProvider.when("/login",{
-            templateUrl:"login.html"
-        }).when("/register",{
-            templateUrl:"register.html"
-        }).when("/profile",{
-            templateUrl:"profile.html"
+        $routeProvider.when("/login", {
+            templateUrl: "login.html"
+        }).when("/register", {
+            templateUrl: "register.html"
+        }).when("/profile/:userId", {
+            templateUrl: "profile.html"
         })
     }
-    function loginController($scope,$location) {
-        var users = [{ _id: 123, username: "alice", password: "alice", firstName: "Alice", lastName: "Wonderland" },
-        { _id: 234, username: "bob", password: "bob", firstName: "Bob", lastName: "Bob" },
+    function loginController($scope, $location) {
+        var users = [
+            { _id: 123, username: "alice", password: "alice", firstName: "Alice", lastName: "Wonderland" },
+            { _id: 234, username: "bob", password: "bob", firstName: "Bob", lastName: "Bob" },
         ];
 
         $scope.login = function (user) {
@@ -26,7 +41,7 @@
                 var _user = users[u];
 
                 if (_user.username === user.username && _user.password === user.password) {
-                    $location.url("profile");
+                    $location.url("profile/" + _user._id);
                 }
             }
             $scope.errorMessage = "User not found.";
