fa4d96e89e2229eefcb69038b9ccaccfc987d50d
Israel <213withani@gmail.com>
2017-10-11 21:39:16 -0700

    Refactored and add now can acces home landing page.

--- assignment/config.js
+++ assignment/config.js
@@ -6,25 +6,31 @@
         .config(configuration);
 
     function configuration($routeProvider) {
-        $routeProvider.when("/login", {
-            templateUrl: "user/templates/login.view.client.html",
+        $routeProvider
+        .when("/", {
+            templateUrl: "views/home.view.client.html"
+        })
+        .when("/login", {
+            templateUrl: "views/user/templates/login.view.client.html",
             controller: "loginController",
             controllerAs: "model"
-        }).when("/register", {
-            templateUrl: "user/templates/register.view.client.html",
+        })
+        .when("/register", {
+            templateUrl: "views/user/templates/register.view.client.html",
             controller: "registerController",
             controllerAs: "model"
-        }).when("/profile/:userId", {
-            templateUrl: "user/templates/profile.view.client.html",
+        })
+        .when("/profile/:userId", {
+            templateUrl: "views/user/templates/profile.view.client.html",
             controller: "profileController",
             controllerAs: "model"
         })
             // Website routes
-            .when("/user/:userId/website", {
+            .when("views//user/:userId/website", {
                 templateUrl: "website/templates/website-list.view.client.html",
                 controller: "websiteListController",
                 controllerAs: "model"
-            }).when("/user/:userId/website/new", {
+            }).when("views//user/:userId/website/new", {
                 templateUrl: "website/templates/website-new.view.client.html"
                 // controller: "websiteListController",
                 // controllerAs: "model"
