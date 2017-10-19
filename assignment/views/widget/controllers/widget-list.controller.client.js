c27f99bc189823d10953feff68ba708c50d99aea
Israel <213withani@gmail.com>
2017-10-18 19:53:47 -0700

    Created widgets array with test data

--- assignment/views/widget/controllers/widget-list.controller.client.js
+++ assignment/views/widget/controllers/widget-list.controller.client.js
@@ -7,6 +7,12 @@
         var model = this;
         function init() {
             model.hello = "Hello from widget";
+            model.widgets =
+            [
+                { "_id": "123", "WidgetType": "HEADING", "pageId": "321", "size": 2, "text": "GIZMODO" },
+                { "_id": "234", "WidgetType": "HEADING", "pageId": "321", "size": 4, "text": "GIZMODO" },
+                { "_id": "345", "WidgetType": "IMAGE", "pageId": "321", "width": "100%", "url": "http://lorempixel.com/400/200" }
+            ];
         }
         init();
     }
