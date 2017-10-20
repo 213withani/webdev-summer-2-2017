5c9173d031dad7076c27f83fd877ba721658d320
Israel <213withani@gmail.com>
2017-10-20 00:30:15 -0700

    Started using $sce to secure data

--- assignment/views/widget/controllers/widget-list.controller.client.js
+++ assignment/views/widget/controllers/widget-list.controller.client.js
@@ -3,17 +3,23 @@
         .module("WampApp")
         .controller("widgetListController", widgetListController);
 
-    function widgetListController() {
+    function widgetListController($sce) {
         var model = this;
+        model.trustHtmlContent = trustHtmlContent;
+        
         function init() {
-            model.hello = "Hello from widget";
+            model.hello = "Hello from widgetListController";
             model.widgets =
-            [
-                { "_id": "123", "widgetType": "HEADING", "pageId": "321", "size": 2, "text": "GIZMODO" },
-                { "_id": "234", "widgetType": "HEADING", "pageId": "321", "size": 4, "text": "GIZMODO" },
-                { "_id": "345", "widgetType": "IMAGE", "pageId": "321", "width": "100%", "url": "http://lorempixel.com/400/200" }
-            ];
+                [
+                    { "_id": "123", "widgetType": "HEADING", "pageId": "321", "size": 2, "text": "GIZMODO" },
+                    { "_id": "234", "widgetType": "HTML", "pageId": "321", "size": 4, "text": "<p>GIZMODO</p>" },
+                    { "_id": "345", "widgetType": "IMAGE", "pageId": "321", "width": "100%", "url": "http://lorempixel.com/400/200" }
+                ];
         }
         init();
+
+        function trustHtmlContent(htmlContent) {
+            return $sce.trustAsHtml(htmlContent);
+        }
     }
 })();
