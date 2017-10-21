c60f4135542a570cbf56d0908eef08692337b43e
Israel <213withani@gmail.com>
2017-10-21 09:48:42 -0700

    Added trusted video url using $sce

--- assignment/views/widget/controllers/widget-list.controller.client.js
+++ assignment/views/widget/controllers/widget-list.controller.client.js
@@ -6,20 +6,27 @@
     function widgetListController($sce) {
         var model = this;
         model.trustHtmlContent = trustHtmlContent;
-        
+        model.trustUrlResource = trustUrlResource;
         function init() {
             model.hello = "Hello from widgetListController";
             model.widgets =
                 [
                     { "_id": "123", "widgetType": "HEADING", "pageId": "321", "size": 2, "text": "GIZMODO" },
                     { "_id": "234", "widgetType": "HTML", "pageId": "321", "size": 4, "text": "<p>GIZMODO</p>" },
-                    { "_id": "345", "widgetType": "IMAGE", "pageId": "321", "width": "100%", "url": "http://lorempixel.com/400/200" }
+                    { "_id": "345", "widgetType": "IMAGE", "pageId": "321", "width": "100%", "url": "http://lorempixel.com/400/200" },
+                    { "_id": "456", "widgetType": "YOUTUBE", "pageId": "321", "size": 2, "text": "GIZMODO", "url": "https://www.youtube.com/embed/YH5sFprHgl0" }
                 ];
         }
         init();
-
+        function trustUrlResource(url) {
+            console.log(url);
+            var youtubeUrl = "https://www.youtube.com/embed/";
+            var urlParts = url.split("/");
+            youtubeUrl += urlParts[urlParts.length - 1];
+            return $sce.trustAsResourceUrl(youtubeUrl);
+        }
         function trustHtmlContent(htmlContent) {
-            return $sce.trustAsHtml(htmlContent);
+            return $sce.trustAsHtml (htmlContent);
         }
     }
 })();
